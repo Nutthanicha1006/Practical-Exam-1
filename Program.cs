81e0fa321c940e6f7eb77f2fbc52fe361b65fd89
LAPTOP-2QNNL6VT\Lenovo <nutthanicha.kmutt@mail.kmutt.ac.th>
2021-03-28 15:05:46 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp2TEST00._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+
+            Console.Write("Input a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input b : ");
+            b = int.Parse(Console.ReadLine());
+
+            x = a;
+            y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine("{0}",x);
+
+            Console.ReadLine();
+
+        }
+    }
+}
